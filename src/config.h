d2939e46e03981643e8cd6ced1ac5b8ad95b3820
guido <guido@dogsally.com>
2024-03-08 20:20:10 +0100

    Initial commit

---
+++ src/config.h
@@ -0,0 +1,21 @@
+#ifndef __BELL_STRUCT__
+#define __BELL_STRUCT__
+
+
+struct bell_struct {
+    int pin;
+    int note;
+    int target;
+    int rest;
+    int target_time_ms;
+    int rest_time_ms;
+  };
+
+struct config_struct {
+  
+  bell_struct Bells[6];
+  long mp3_to_smf_delay_ms;
+  int volume;
+};
+
+#endif
